310f00715444285a45aaf638e77e3df57ec4199e
Dennis Cosgrove <cosgroved@wustl.edu>
2018-03-27 12:11:47 -0500

    replaced __STUDENT_NAME__ with Yiheng Huang

--- src/main/java/sudoku/lab/DefaultConstraintPropagator.java
+++ src/main/java/sudoku/lab/DefaultConstraintPropagator.java
@@ -40,7 +40,7 @@ import sudoku.core.io.PuzzlesResourceUtils;
  * Constraint propagation approach and terminology adopted from
  * <a href="http://norvig.com/sudoku.html">Peter Norvig's essay</a>.
  * 
- * @author __STUDENT_NAME__
+ * @author Yiheng Huang
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
 @Immutable
