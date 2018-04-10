6cb1bde9b20757dac019454cc1fb9874836d6ecf
Dennis Cosgrove <cosgroved@wustl.edu>
2018-04-10 01:38:07 -0500

    replaced __STUDENT_NAME__ with Yiheng Huang

--- src/main/java/kmer/warmup/stringmap/StringConcurrentHashMapKMerCounter.java
+++ src/main/java/kmer/warmup/stringmap/StringConcurrentHashMapKMerCounter.java
@@ -43,7 +43,7 @@ import slice.core.Slice;
  * A parallel implementation of {@link KMerCounter} that uses a
  * {@link ConcurrentHashMap}, where each k-mer is represented as a String.
  * 
- * @author __STUDENT_NAME__
+ * @author Yiheng Huang
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
 public class StringConcurrentHashMapKMerCounter implements KMerCounter {
