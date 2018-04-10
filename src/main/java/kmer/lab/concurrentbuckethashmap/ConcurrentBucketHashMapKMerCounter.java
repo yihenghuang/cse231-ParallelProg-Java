370ff114a09cd90a4b2f79606fe4d39438945781
Dennis Cosgrove <cosgroved@wustl.edu>
2018-04-10 01:38:07 -0500

    replaced __STUDENT_NAME__ with Yiheng Huang

--- src/main/java/kmer/lab/concurrentbuckethashmap/ConcurrentBucketHashMapKMerCounter.java
+++ src/main/java/kmer/lab/concurrentbuckethashmap/ConcurrentBucketHashMapKMerCounter.java
@@ -36,7 +36,7 @@ import kmer.lab.util.ThresholdSlices;
 import slice.core.Slice;
 
 /**
- * @author __STUDENT_NAME__
+ * @author Yiheng Huang
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
 public class ConcurrentBucketHashMapKMerCounter implements KMerCounter {
